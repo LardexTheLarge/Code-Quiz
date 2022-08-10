3427f25a65901a62c5b273cb1063563c0e918cd6
Gabe Vee <0gabevee0@gmail.com>
2022-08-10 17:33:25 -0600

    added a timer function

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,9 @@ var li3 = document.createElement("li");
 var startBtn = document.createElement("button");
 var introText = document.createElement("p");
 
+//timer
+var timerEl = document.getElementById("time");
+
 document.body.appendChild(startDiv);
 startDiv.appendChild(introText);
 startDiv.appendChild(startBtn);
@@ -28,19 +31,19 @@ h1El.setAttribute("style", "background-color: #2b2bff; margin:0;");
 listEl.setAttribute("style", "list-style: none; display:inline;");
 li1.setAttribute(
   "style",
-  "border: 1px solid #040080; text-align:center; margin: 2px 0; color: white;"
+  "border: 1px solid #005eff; text-align:center; margin: 2px 0; color: white; cursor: pointer;"
 );
 li2.setAttribute(
   "style",
-  "border: 1px solid #040080; text-align:center; margin: 2px 0; color: white;"
+  "border: 1px solid #005eff; text-align:center; margin: 2px 0; color: white; cursor: pointer;"
 );
 li3.setAttribute(
   "style",
-  "border: 1px solid #040080; text-align:center; margin: 2px 0; color: white;"
+  "border: 1px solid #005eff; text-align:center; margin: 2px 0; color: white; cursor: pointer;"
 );
 startBtn.setAttribute(
   "style",
-  "width: 50px; height: 20px; color: white; background-color: blue; "
+  "width: 50px; height: 20px; color: white; background-color: blue; cursor: pointer;"
 );
 startDiv.setAttribute(
   "style",
@@ -49,48 +52,48 @@ startDiv.setAttribute(
 
 var testQuestions = [
   {
-    question: "Which js function intervals time?",
+    question: "Godzilla translation in Japanese:__________",
     answers: {
-      a: "log()",
-      b: "clearInterval()",
-      c: "setInterval()",
+      a: "メカゴジラ",
+      b: "スペースゴジラ",
+      c: "ゴジラ",
     },
     correctAnswer: "c",
   },
   {
-    question: "Which Library is an extension of Javascript?",
+    question: "How did Godzilla's story come to be created?",
     answers: {
-      a: "BootStrap",
-      b: "TypeScript",
-      c: "JQuery",
+      a: "A story the director came up with",
+      b: "WWII when Japan got nuked by the US military",
+      c: "Godzilla is a real story",
     },
-    correctAnswer: "c",
+    correctAnswer: "b",
   },
   {
-    question: "How do you repeat code?",
+    question: "Who is Godzilla's friend?",
     answers: {
-      a: "for loops",
-      b: "if statements",
-      c: "Console.log",
+      a: "Angirus",
+      b: "King Ghidorah",
+      c: "King Kong",
     },
     correctAnswer: "a",
   },
   {
-    question: "How do you connect html to Javascript?\ndocument.________",
+    question: "The first Godzilla movie aired in what year?",
     answers: {
-      a: "appendChild()",
-      b: "querySelector()",
-      c: "innerHtml()",
+      a: "2014",
+      b: "1954",
+      c: "1956",
     },
     correctAnswer: "b",
   },
 ];
 
-var gIndex = 0;
-for (let i = 0; i < testQuestions[gIndex].answers.length; i++) {
-  // Code to create button elements, add text based off of
-  testQuestions[gIndex].answers[i];
-}
+// var gIndex = 0;
+// for (let i = 0; i < testQuestions[gIndex].answers.length; i++) {
+// //   Code to create button elements, add text based off of
+//   testQuestions[gIndex].answers[i];
+// }
 
 function questionPrompt() {
   document.body.appendChild(mainEl);
@@ -101,23 +104,63 @@ function questionPrompt() {
   listEl.appendChild(li3);
 
   for (var i = 0; i < testQuestions.length; i++) {
-    h1El.textContent = testQuestions[0].question;
-    li1.textContent = testQuestions[0].answers.a;
-    li2.textContent = testQuestions[0].answers.b;
-    li3.textContent = testQuestions[0].answers.c;
+    h1El.textContent = testQuestions[i].question;
+    li1.textContent = testQuestions[i].answers.a;
+    li2.textContent = testQuestions[i].answers.b;
+    li3.textContent = testQuestions[i].answers.c;
+    console.log(testQuestions[i].answers);
   }
+
+  // btn eventlistener
+  li1.addEventListener("click", function () {
+    testQuestions[i].correctAnswer === testQuestions[i].answers.a
+      ? alert("right answer")
+      : alert("wrong Answer -10 seconds");
+  });
+  li2.addEventListener("click", function () {
+    testQuestions[i].correctAnswer === testQuestions[i].answers.b
+      ? alert("right answer")
+      : alert("wrong Answer -10 seconds");
+  });
+  li3.addEventListener("click", function () {
+    testQuestions[i].correctAnswer === testQuestions[i].answers.c
+      ? alert("right answer")
+      : alert("wrong Answer -10 seconds");
+  });
 
   introText.textContent = "";
   startBtn.setAttribute("style", "display: none;");
 }
 
+//This function calls to the questions and starts the test
 function start() {
   introText.textContent = "Press Start to begin test";
   startBtn.textContent = "Start";
 
   // starts test
   startBtn.addEventListener("click", function () {
+    //TODO: add timer
+    // countdown();
     questionPrompt();
   });
 }
 start();
+
+function countdown() {
+  var timeLeft = 50;
+
+  var timeInterval = setInterval(function () {
+    //
+    // YOUR CODE HERE
+    //
+    if (timeLeft >= 1) {
+      timeLeft--;
+      timerEl.textContent = "Time: " + timeLeft;
+      console.log(timeLeft);
+    } else {
+      timerEl.textContent = " ";
+      // Stops execution of action at set interval
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
